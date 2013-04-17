1aa326435e7cd66b3a7e06231251cb7e6ed4aa16
srifqi <muhammadrifqipriyosusanto@gmail.com>
2013-04-17 20:12:29 +0700

    Update calendar.js
    bug fixes on multiple calendar

--- calendar.js
+++ calendar.js
@@ -4,16 +4,17 @@
 **/
 
 /* define CAL */
-var CAL = CAL || {};
+var CAL = CAL || {REVISION: 2};
 
 /* global variable */
 CAL.Month = ["January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 CAL.Month.numberOfDays = [31, 28, 31, 30, 31, 30, 31, 30, 31, 30, 31, 30];
 CAL.Days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+CAL.CalendarCount = 0;
 
 /* core calendar process */
 CAL.Calendar = function(year,month,place) {
-  var table = "<table id=\"CAL-Calendar\">" +
+	var table = "<table id=\"CAL-Calendar-" + CAL.CalendarCount + "\">" +
 				"<thead><tr><th colspan=\"7\"><big><big>" + CAL.Month[month] + " " + Number(year) + "</big></big></th></tr></thead>" +
 				"<tbody>\n" +
 				"<tr><th></th><th></th><th></th><th></th><th></th><th></th><th></th></tr>\n" +
@@ -23,9 +24,9 @@ CAL.Calendar = function(year,month,place) {
 				"<tr><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>\n" +
 				"<tr><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>\n" +
 				"</tbody></table>\n" +
-				"<style>#CAL-Calendar {width: 70%; font-family: Arial; text-align: center;}\n#CAL-Calendar tr th {width: 10%; font-size: 150%; border: 1px solid black;}\n#CAL-Calendar tr td {width: 10%; font-size: 200%; border: 1px solid black;}\n</style>";
+				"<style>#CAL-Calendar-" + CAL.CalendarCount + " {width: 70%; font-family: Arial; text-align: center;}\n#CAL-Calendar-" + CAL.CalendarCount + " tr th {width: 10%; font-size: 150%; border: 1px solid black;}\n#CAL-Calendar-" + CAL.CalendarCount + " tr td {width: 10%; font-size: 200%; border: 1px solid black;}\n</style>";
 	document.querySelector(place).innerHTML += table;
-	var CAL_div = document.getElementById("CAL-Calendar");
+	var CAL_div = document.getElementById("CAL-Calendar-" + CAL.CalendarCount);
 	for(i=0;i<CAL.Days.length;) {
 		CAL_div.querySelectorAll("tbody > tr > th")[i].innerHTML = CAL.Days[i][0] + CAL.Days[i][1] + CAL.Days[i][2];
 		i++;
@@ -51,4 +52,8 @@ CAL.Calendar = function(year,month,place) {
 		}
 		r++;
 	}
+	
+	CAL.CalendarCount++;
 };
+
+console.log("calendar.js loaded",CAL.REVISION);
