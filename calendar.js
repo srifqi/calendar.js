c371802b10d3a6746a450e39723177a94e5cd778
srifqi <muhammadrifqipriyosusanto@gmail.com>
2014-03-23 14:48:19 +0700

    r3
    smaller size
    more computation

--- calendar.js
+++ calendar.js
@@ -4,7 +4,7 @@
 **/
 
 /* define CAL */
-var CAL = CAL || {REVISION: 2};
+var CAL = CAL || {REVISION: 3};
 
 /* global variable */
 CAL.Month = ["January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
@@ -14,17 +14,23 @@ CAL.CalendarCount = 0;
 
 /* core calendar process */
 CAL.Calendar = function(year,month,place) {
+	console.log("CAL.Calendar",CAL.REVISION);
+	var month = (month - 1); //january = 1
 	var table = "<table id=\"CAL-Calendar-" + CAL.CalendarCount + "\">" +
 				"<thead><tr><th colspan=\"7\"><big><big>" + CAL.Month[month] + " " + Number(year) + "</big></big></th></tr></thead>" +
-				"<tbody>\n" +
-				"<tr><th></th><th></th><th></th><th></th><th></th><th></th><th></th></tr>\n" +
-				"<tr><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>\n" +
-				"<tr><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>\n" +
-				"<tr><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>\n" +
-				"<tr><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>\n" +
-				"<tr><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>\n" +
-				"</tbody></table>\n" +
-				"<style>#CAL-Calendar-" + CAL.CalendarCount + " {width: 70%; font-family: Arial; text-align: center;}\n#CAL-Calendar-" + CAL.CalendarCount + " tr th {width: 10%; font-size: 150%; border: 1px solid black;}\n#CAL-Calendar-" + CAL.CalendarCount + " tr td {width: 10%; font-size: 200%; border: 1px solid black;}\n</style>";
+				"<tbody>" +
+				"<tr>";
+	for(var i=0;i<7;i++){table+= "<th></th>"}
+	table += "</tr>";
+	for(var i=0;i<5;i++) {
+		table += "<tr>";
+		for(var o=0;o<7;o++) {
+			table += "<td></td>";
+		}
+		table += "</tr>";
+	}
+	table += "</tbody></table>" +
+			"<style>#CAL-Calendar-" + CAL.CalendarCount + " {width: 70%; font-family: Arial; text-align: center;}\n#CAL-Calendar-" + CAL.CalendarCount + " tr th {width: 10%; font-size: 150%; border: 1px solid black;}\n#CAL-Calendar-" + CAL.CalendarCount + " tr td {width: 10%; font-size: 200%; border: 1px solid black;}</style>";
 	document.querySelector(place).innerHTML += table;
 	var CAL_div = document.getElementById("CAL-Calendar-" + CAL.CalendarCount);
 	for(i=0;i<CAL.Days.length;) {
@@ -34,23 +40,18 @@ CAL.Calendar = function(year,month,place) {
 	
 	var dn = 1;
 	
-	for(r=1;r<6;) {
-		for(d=0;d<7;) {
-			if(r===1 && d<Number(new Date(year,month,1).getDay())) {
-				/*blablabla*/
-			} else if(dn>CAL.Month.numberOfDays[month]) {
-				/*blablabla*/
-			} else {
-				var qw = "",
-					qe = "";
-				if(d===5) {qw = "<span style=\"color: green;\">"; qe = "</span>"}
-				if(d===0) {qw = "<span style=\"color: red;\">"; qe = "</span>"}
-				CAL_div.querySelectorAll("tbody > tr")[r].querySelectorAll("td")[d].innerHTML += qw + dn + qe;
-				dn++;
-			}
-			d++;
+	for(r=1;r<6;r++) {
+		for(d=0;d<7;d++) {
+			if(r===1 && d<Number(new Date(year,month,1).getDay())) continue;
+			if(dn>CAL.Month.numberOfDays[month]) continue;
+			
+			var qw = "",
+				qe = "";
+			if(d===5) {qw = "<span style=\"color: green;\">"; qe = "</span>"}
+			if(d===0) {qw = "<span style=\"color: red;\">"; qe = "</span>"}
+			CAL_div.querySelectorAll("tbody > tr")[r].querySelectorAll("td")[d].innerHTML += qw + dn + qe;
+			dn++;
 		}
-		r++;
 	}
 	
 	CAL.CalendarCount++;
